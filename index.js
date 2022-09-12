b7fd4d96c5802b350488e116ee92851606372dc9
azeembokaro <64311194+azeembokaro@users.noreply.github.com>
2022-09-12 18:50:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+let screen = document.getElementById('screen');
+let buttons = document.querySelectorAll('button');
+let screenValue="";
+for(let items of buttons){
+    items.addEventListener('click',(e)=>{
+        let buttonText=e.target.innerText;
+        // console.log(buttonText);
+        
+        if(buttonText=='C'){
+            screenValue=""
+            screen.value=screenValue;
+           
+        }else if(buttonText == '='){
+            screen.value=eval(screenValue);
+        }else{
+            screenValue+=buttonText;
+            screen.value=screenValue;
+        }
+    })
+}
